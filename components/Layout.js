343c61c0497e6edd5ff122c0175a0a3d199f1dcf
pudron <pudron@foxmail.com>
2023-02-28 13:52:39 +0800

    add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import { useEffect } from 'react';
 import styles from './Layout.module.css';
+import Head from 'next/head';
 
 export function GradientBackground({ variant, className }) {
   const classes = classNames(
@@ -50,10 +51,21 @@ export default function Layout({ children }) {
   }, []);
 
   return (
-    <div className="relative pb-24 overflow-hidden">
-      <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
-        {children}
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <div className="relative pb-24 overflow-hidden">
+        <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
